bba4f3eaa1d18dc551696e76e10d07f6cf102016
Adam Menkes <amenkes24@gmail.com>
2022-03-23 10:36:35 -0700

    fill out about me page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,8 +4,10 @@ import headshot from "../../assets/cover/headshotCircle.png";
 function About() {
   return (
     <section className='my-5'>
-      <h1 id="about">Who am I?</h1>
-      <img src={headshot} className="my-2" style={{ width: "50%" }} alt="headshot" />
+        <img src={headshot} className="my-2" style={{ width: "25%" }} alt="headshot" />
+        <h1 id="about">Adam Menkes</h1>
+        <p>Hi, my name is Adam, and I'm an aspiring full stack web developer! I'm a recent graduate of the UC Berkeley Coding Bootcamp, and I am currently living in San Francisco, CA.</p>
+        <p>I was a former Project Engineer with an architecture firm; however I was drawn to the tech industry after living in San Francisco, and decided it was time to make a change with my career.</p>
     </section>
   );
 }
